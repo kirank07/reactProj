6184eb76c5edbeb9b1be431ade18adf751c42154
Kiran Khade <53072447+kirank07@users.noreply.github.com>
2023-02-28 22:24:59 +0530

    Update usercontroller.js

--- server/controller/usercontroller.js
+++ server/controller/usercontroller.js
@@ -1,1 +1,14 @@
+import users from '../model/usermodel.js';
 
+export const addUser = async (request,response) => {
+    const user = request.body;
+    console.log(user);
+
+    const newUser = new users(user);
+    try {
+        await newUser.save();
+        response.status(201).json(newUser);
+    } catch (error) {
+        response.status(409).json(error);
+    }
+}
